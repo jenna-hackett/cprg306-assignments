e9b4eed2f207e05bed9c64c8b84f29ad5b79e0d7
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-16 16:08:34 -0700

    Changed ANCHOR tag to LINK tag as per instructions. Made some changes to the html elements.

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -2,6 +2,8 @@
 Versions: Jan. 16, 2026
  */
 
+import Link from "next/link";
+
 export default function StudentInfo() {
   return (
     <section>
@@ -10,7 +12,12 @@ export default function StudentInfo() {
         <h2>Student Information</h2>
       </header>
       <p>Student Name: Jenna Hackett</p>
-      <p>GitHub Link: <a href="https://github.com/jenna-hackett/cprg306-assignments">CPRG306-Assignments</a></p>
+      <p>
+        GitHub Link:{" "}
+        <Link href="https://github.com/jenna-hackett/cprg306-assignments">
+          CPRG306-Assignments
+        </Link>
+      </p>
     </section>
 );
 }
