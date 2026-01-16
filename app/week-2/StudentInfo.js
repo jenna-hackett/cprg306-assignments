713aa246892e85e478ae26cc8e7f602043624b54
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-16 15:47:43 -0700

    page.js and StudentInfo.js added. Student info function has been completed (no tailwind styling yet)

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,16 @@
+/* Author: Jenna Hackett
+Versions: Jan. 16, 2026
+ */
+
+export default function StudentInfo() {
+  return (
+    <section>
+      {/* Student Info Component */}
+      <header>
+        <h2>Student Information</h2>
+      </header>
+      <p>Student Name: Jenna Hackett</p>
+      <p>GitHub Link: <a href="https://github.com/jenna-hackett/cprg306-assignments">CPRG306-Assignments</a></p>
+    </section>
+);
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+/* Author: Jenna Hackett
+Versions: Jan. 16, 2026
+ */
+
+import StudentInfo from "./StudentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo /> 
+    </main>
+  );
+}
