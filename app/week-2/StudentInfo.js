ec77d6fd3df6a780b1686ba69443901e437adcd2
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-27 08:44:12 -0700

    made changes to CSS in StudentInfo.js

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -10,16 +10,16 @@ export default function StudentInfo() {
   return (
     <section>
       {/* Student Info Component */}
-      <header>
-        <h1 className="p-4 mb-15 text-3xl text-center font-bold text-pink-300">
+      <header className="p-4 mb-15 text-3xl text-center font-bold text-pink-300">
+        <h1>
           Student Information
         </h1>
       </header>
-      <section>
-        <p className="pl-8 pb-6 text-xl text-pink-300">
+      <section className="pl-8 pb-6 text-xl text-pink-300">
+        <p className = "pb-6">
           Name: Jenna Hackett
         </p>
-        <p className="pl-8 text-xl text-pink-300">
+        <p className = "pb-6">
           GitHub Link:
             <Link className=" hover:text-pink-700 duration-300 ease-linear" 
               href="https://github.com/jenna-hackett/cprg306-assignments"
