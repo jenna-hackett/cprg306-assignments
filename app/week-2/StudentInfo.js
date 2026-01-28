97368050d4ac80edbccd516a5f1b3e16e5ed2cac
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-27 17:13:24 -0700

    changed font size of student name inside studentInfo component.

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -10,7 +10,7 @@ import { linkInfo } from "../components/LinkInformation";
 export default function StudentInfo({name}) {
   return (
     <section className="px-4 py-4 border-amber-500 border-2 rounded-md max-w-md mx-4 mt-6">
-        <h2 className="text-2xl font-bold text-indigo-900 dark:text-indigo-400 mb-2">
+        <h2 className="text-xl font-bold text-indigo-900 dark:text-indigo-400 mb-2">
           Name: <span className="text-slate-900 dark:text-white">{name}</span>
         </h2>
         <p className="text-slate-900 dark:text-white">
