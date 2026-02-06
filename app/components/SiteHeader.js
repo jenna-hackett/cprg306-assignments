951252d8673d02f72ebec4a47db05a26df962c9c
Jenna Hackett <jenna.hackett96@gmail.com>
2026-02-06 12:19:46 -0700

    switched location of the home button and the cprg header title for functionality purposes.

--- app/components/SiteHeader.js
+++ app/components/SiteHeader.js
@@ -5,15 +5,15 @@ import PageLinks from "./PageLinks";
 export default function SiteHeader() {
   return (
     <header className = "flex gap-4 py-4 px-4 justify-between bg-indigo-900">
-      <p className = "font-bold text-amber-500">
-          CPRG306: Web Development II
-      </p>
       <nav>
         <PageLinks
           href = {linkInfo[0].href}
           text = {linkInfo[0].text}
         />
       </nav>
+      <p className = "font-bold text-amber-500">
+          CPRG306: Web Development II
+      </p>
     </header>
   )
 }
