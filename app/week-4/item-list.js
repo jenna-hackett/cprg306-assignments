8b4adbe68688cda1556eb2d100ec2de3f5e89e9a
Jenna Hackett <jenna.hackett96@gmail.com>
2026-02-09 08:31:09 -0700

    changes implemented for week 4 based on instructor feedback from week 3.

--- app/week-4/item-list.js
+++ app/week-4/item-list.js
@@ -11,29 +11,31 @@ export default function ItemList() {
 
   return (
     <main className="p-4 text-center">
-      
-      {sortedItems.map((item, index) => {
-        // Show category header if it's the first item OR if the category is different from the previous one
-        const isFirstOfCategory = index === 0 || item.category !== sortedItems[index - 1].category;
+      <ul className="list-none p-0">
+        {sortedItems.map((item, index) => {
+          // Show category header if it's the first item OR if the category is different from the previous one
+          const isFirstOfCategory = index === 0 || item.category !== sortedItems[index - 1].category;
 
-        return (
-          <div key={item.id}>
-            {/* Render the category title when it's the first of its kind */}
-            {isFirstOfCategory && (
-              <h2 className="text-xl font-bold text-pink-700 mb-4 mt-2">
-                {item.category}
-              </h2>
-            )}
+          return (
+            <div key={item.id}>
+              {/* Render the category title when it's the first of its kind */}
+              {isFirstOfCategory && (
+                <section>
+                  <h2 className="text-xl font-bold text-pink-700 mb-4 mt-2">
+                    {item.category}
+                  </h2>
+                </section>
+              )}
 
-            {/* Render the Item component */}
-            <Item 
-              name={item.name} 
-              quantity={item.quantity} 
-              category={item.category} 
-            />
-          </div>
+              {/* Render the Item component */}
+              <Item 
+                name={item.name} 
+                quantity={item.quantity} 
+              />
+            </div>
         );
       })}
+      </ul>
     </main>
   );
 }
--- app/week-4/item.js
+++ app/week-4/item.js
@@ -1,11 +1,9 @@
 
 export default function Item({name, quantity}) {
   return (
-    <section>
-      <ul className = "border-rose-400 border-4 rounded-lg max-w-md mx-auto p-5 mb-4 bg-pink-900">
-        <li className = "font-bold text-pink-200 text-xl">{name}</li>
-        <li className="text-white">Quantity: {quantity}</li>
-      </ul>
-    </section>
+    <li className = "border-rose-400 border-4 rounded-lg max-w-md mx-auto p-5 mb-4 bg-pink-900">
+      <h3 className = "font-bold text-pink-200 text-xl">{name}</h3>
+      <p className="text-white">Quantity: {quantity}</p>
+    </li>
   );
 }
