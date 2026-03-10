6b834be6f6c83fe58a1cedd1fc89e582229f24f0
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-09 19:49:27 -0600

    copied files from week 7folder into week 8 folder to start week 8 assignment.

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,17 +1,29 @@
 "use client";
+import { useState } from "react";
 import SiteHeader from "../components/SiteHeader";
 import PageHeader from "../components/PageHeader";
+import itemData from "./grocery-items.json";
+import NewItem from "./NewGroceryItem";
+import ItemList from "./GroceryItemList";
 
 export default function Page() {
+  const [items, setItems] = useState(itemData);
+
+  function handleAddItem(newItem) {
+    setItems((prev) => [...prev, newItem]);
+  }
+
   return (
     <main>
       <header>
         <SiteHeader />
         <PageHeader 
-          title="Week 8 "
-          description="Week 8 assignment."
+          title="Managing State"
+          description="Merging the functionalities developed during Week 4, 5 and 6 into a single, interactive shopping list application. The application will consist of a form for adding new items and a sortable list for displaying these items."
         />
       </header>
+      <NewItem onAddItem={handleAddItem}/>
+      <ItemList items={items}/>
     </main>
   )
 }
