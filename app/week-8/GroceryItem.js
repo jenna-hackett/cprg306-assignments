0217d3427841258b1c65428acfa0f6e664bb4b3e
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-09 20:53:52 -0600

    changes made to page.js and styling for the page completed.

--- app/week-8/GroceryItem.js
+++ app/week-8/GroceryItem.js
@@ -3,7 +3,7 @@ export default function Item({name, quantity, category, hideCategory, onSelect})
   return (
     <section>
       <div 
-      className = "border-rose-400 border-4 rounded-lg max-w-md mx-auto p-5 mb-4 bg-pink-900"
+      className = "border-rose-400 border-4 rounded-lg max-w-md mx-auto p-5 mb-4 bg-pink-900 cursor-pointer"
       onClick={() => onSelect(name)}
       > 
         <h3 className = "font-bold text-pink-200 text-lg">{name}</h3>
--- app/week-8/MealIdeas.js
+++ app/week-8/MealIdeas.js
@@ -34,13 +34,35 @@ export default function MealIdeas({ ingredient }) {
 
 
   return (
-    <div>
-      <h2>Meal Ideas for {ingredient}</h2>
-      <ul>
-        {meals.map((meal) => (
-          <li key={meal.idMeal}>{meal.strMeal}</li>
-        ))}
-      </ul>
+    <div className="bg-pink-900 border-rose-400 border-2 rounded-lg p-5 mb-4 max-w-md mt-9 mx-auto">
+      <h2 className="text-2xl font-bold text-pink-200 mb-2">Meal Ideas</h2>
+      {/* No ingredient is selected */}
+      {!ingredient ? (
+        <p className="text-white mt-2 italic">Select an item from the list to see meal ideas</p>
+      ) : (
+        <>
+          <p className="text-white mt-2 mb-4">
+            Here are some meal ideas using <span className="font-bold text-pink-200">`{ingredient}`</span>:
+          </p>
+          
+          {/* Ingredient is selected but no meals found. */}
+          {meals.length === 0 ? (
+            <p className="text-pink-300 italic">No meal ideas found for this ingredient.</p>
+          ) : (
+            /* Meal List */
+            <ul className="space-y-3">
+              {meals.map((meal) => (
+                <li 
+                  key={meal.idMeal} 
+                  className="p-3 bg-pink-800 border-2 border-rose-300 rounded-md text-white hover:bg-pink-700 transition-colors cursor-pointer"
+                >
+                  {meal.strMeal}
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
     </div>
   );
 }
