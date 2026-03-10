6fb783fa28939efa7542886056428c6f1816c83f
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-10 09:46:16 -0600

    landing page for week-9 created and skeleton code pasted into it from instructor webpage.

---
+++ app/week-9/page.js
@@ -0,0 +1,20 @@
+// Display a login button if the user is not logged in. 
+// If the user is logged in, display a welcome message, a logout button, and a link to the shopping list page.
+
+// BELOW IS SKELETON CODE PROVIDED BY INSTRUCTOR.
+// Import the useUserAuth hook
+import { useUserAuth } from "../contexts/AuthContext";
+ 
+// Use the useUserAuth hook to get the user object and the login and logout functions
+//const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+ 
+// Sign in to Firebase with GitHub authentication
+await gitHubSignIn();
+ 
+// Sign out of Firebase
+await firebaseSignOut();
+ 
+// Display some of the user's information
+<p>
+  Welcome, {user.displayName} ({user.email})
+</p>;
