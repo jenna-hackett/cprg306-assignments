dcce8ebc085d12700bdbcc594445b14c26d2bae3
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-13 09:51:25 -0600

    enclosed  {children} in layout.js with <AuthContextProvider>

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-//import { AuthContextProvider } from "./contexts/AuthContext.js";
+import { AuthContextProvider } from "./contexts/AuthContext.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,8 +23,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* Render the actual page content here */}
-        {children} 
+        <AuthContextProvider>{children}</AuthContextProvider> 
       </body>
     </html>
   );
