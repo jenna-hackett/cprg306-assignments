6ef4628e252b731ec414a92ea8b2af767cdd9c8b
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-13 08:51:26 -0600

    authcontext also commented out for the time being.

--- app/contexts/AuthContext.js
+++ app/contexts/AuthContext.js
@@ -1,42 +1,42 @@
-"use client";
+// "use client";
  
-import { useContext, createContext, useState, useEffect } from "react";
-import {
-  signInWithPopup,
-  signOut,
-  onAuthStateChanged,
-  GithubAuthProvider,
-} from "firebase/auth";
-import { auth } from "../utils/firebase";
+// import { useContext, createContext, useState, useEffect } from "react";
+// import {
+//   signInWithPopup,
+//   signOut,
+//   onAuthStateChanged,
+//   GithubAuthProvider,
+// } from "firebase/auth";
+// import { auth } from "../utils/firebase";
  
-const AuthContext = createContext();
+// const AuthContext = createContext();
  
-export const AuthContextProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+// export const AuthContextProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
  
-  const gitHubSignIn = () => {
-    const provider = new GithubAuthProvider();
-    return signInWithPopup(auth, provider);
-  };
+//   const gitHubSignIn = () => {
+//     const provider = new GithubAuthProvider();
+//     return signInWithPopup(auth, provider);
+//   };
  
-  const firebaseSignOut = () => {
-    return signOut(auth);
-  };
+//   const firebaseSignOut = () => {
+//     return signOut(auth);
+//   };
  
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, [user]);
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+//       setUser(currentUser);
+//     });
+//     return () => unsubscribe();
+//   }, [user]);
  
-  return (
-    <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
  
-export const useUserAuth = () => {
-  return useContext(AuthContext);
-};
+// export const useUserAuth = () => {
+//   return useContext(AuthContext);
+// };
