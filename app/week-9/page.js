1dd5b84383dd6fced7f252812cbd1285448f6920
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-13 13:59:35 -0600

    started some styling for week-9 pages.

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,5 +1,7 @@
 "use client";
 import { useUserAuth } from "../contexts/AuthContext";
+import SiteHeader from "@/app/components/SiteHeader";
+import PageHeader from "@/app/components/PageHeader";
 
 export default function Page() {
   // Use the useUserAuth hook to access the user object and authentication functions.
@@ -16,15 +18,33 @@ export default function Page() {
   // Handles if the user is not logged in yet.
   if (!user) {
     return (
-      <div>
-        <p>Please sign in to access the content.</p>
-        <button onClick={handleSignIn}>Sign in with GitHub</button>
-      </div>
+      <main>
+        <header>
+          <SiteHeader />
+          <PageHeader 
+            title="Please sign in to access the content."
+            description="In the Week 9 assignment, firebase authentication is being implemented. A user might sign in using their github account before being able to view the shopping list page of the webpage."
+          />
+        </header>
+
+        <div className="p-4">
+          <button onClick={handleSignIn} className="bg-pink-800 text-white py-2 px-4 rounded cursor-pointer">
+            Sign in with GitHub
+          </button>
+        </div>
+      </main>
     );
   }
 
   return (
     <main>
+      <header>
+        <SiteHeader />
+        <PageHeader 
+          title="Firebase Authentication"
+          description="In the Week 9 assignment, firebase authentication is being implemented. A user might sign in using their github account before being able to view the shopping list page of the webpage."
+          />
+      </header>
       <p>
         Welcome, {user.displayName}!
       </p>
