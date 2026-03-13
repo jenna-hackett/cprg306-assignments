2c0bb6b3971a8a354cf6baf537508124a7d9e167
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-13 13:47:16 -0600

    login started for page.js

--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,19 +1,36 @@
+"use client";
 import { useUserAuth } from "../contexts/AuthContext";
-
 
 export default function Page() {
   // Use the useUserAuth hook to access the user object and authentication functions.
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
+  const handleSignIn = async () => {
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      console.error("Error signing in with GitHub:", error);
+    }
+  }
+
+  // Handles if the user is not logged in yet.
+  if (!user) {
+    return (
+      <div>
+        <p>Please sign in to access the content.</p>
+        <button onClick={handleSignIn}>Sign in with GitHub</button>
+      </div>
+    );
+  }
+
   return (
-    <p>
-      Welcome, {user.displayName}!
-    </p>
+    <main>
+      <p>
+        Welcome, {user.displayName}!
+      </p>
+      <button onClick={firebaseSignOut}>
+        Sign Out
+      </button>
+    </main>
   );
 }
-
-// Sign in to Firebase using GitHub authentication.
-await githubSignIn();
-
-// Sign out of Firebase.
-await firebaseSignOut();
